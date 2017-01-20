78a568bc5139c1e0fd62ef5f78d8a1463ffeab34
Luke Wukusick <lwukusick@gmail.com>
2017-01-19 20:52:56 -0500

    Made play/pause buttons work as well as they can without touching the media player

--- app/src/main/java/edu/rosehulman/boylecj/wmhdonlineradio/MainActivity.java
+++ app/src/main/java/edu/rosehulman/boylecj/wmhdonlineradio/MainActivity.java
@@ -1,28 +1,25 @@
 package edu.rosehulman.boylecj.wmhdonlineradio;
 
-import android.animation.Animator;
 import android.media.AudioManager;
+import android.media.Image;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Layout;
-import android.transition.Slide;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewPropertyAnimator;
+import android.widget.ImageButton;
+
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 import java.io.IOException;
 
@@ -30,7 +27,12 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private MediaPlayer mMediaPlayer;
-    private Toolbar mNowPlayingBar;
+    private ImageButton mPlayPause;
+    private SlidingUpPanelLayout mPanel;
+    private ImageButton mBigPlayPause;
+
+    // Might be able to be replaced by a MediaPlayer function or something
+    private boolean isPlaying;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,12 +49,75 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        mNowPlayingBar = (Toolbar) findViewById(R.id.now_playing_bar);
+
+        isPlaying = true;
+
+        mPlayPause = (ImageButton) findViewById(R.id.play_button);
+
+        mBigPlayPause = (ImageButton) findViewById(R.id.play_button_big);
+
+        setListener((mPlayPause));
+
+        mPanel = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
+        mPanel.addPanelSlideListener(new SlideListener());
 
         if (savedInstanceState == null) {
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             ft.add(R.id.fragment_container, new HomeFragment());
             ft.commit();
+        }
+    }
+
+    // Private listener classes here:
+
+    private class SlideListener implements SlidingUpPanelLayout.PanelSlideListener {
+
+        @Override
+        public void onPanelSlide(View panel, float slideOffset) {
+        }
+
+        @Override
+        public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
+            Log.d("WHMD", "State Changed");
+            if (mPanel.getPanelState() == SlidingUpPanelLayout.PanelState.EXPANDED) {
+                mPlayPause.setOnClickListener(new CloseListener());
+                mPlayPause.setImageResource(R.drawable.ic_expand_more_black_24dp);
+                setListener(mBigPlayPause);
+            } else {
+                //TODO: set listener based on media player state
+                setListener(mPlayPause);
+            }
+        }
+
+    }
+
+    private class CloseListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            mPanel.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+        }
+    }
+
+    private class PlayListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            //TODO: play media player here
+            ImageButton button = (ImageButton)view;
+            button.setImageResource(R.drawable.ic_pause_black_24dp);
+            button.setOnClickListener(new PauseListener());
+            isPlaying = true;
+        }
+    }
+
+    private class PauseListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            //TODO: play media player here
+            ImageButton button = (ImageButton)view;
+            button.setImageResource(R.drawable.ic_play_arrow_black_24dp);
+            button.setOnClickListener(new PlayListener());
+            isPlaying = false;
         }
     }
 
@@ -139,5 +204,15 @@ public class MainActivity extends AppCompatActivity
         ft.replace(R.id.fragment_container, new HomeFragment());
         ft.addToBackStack("home");
         ft.commit();
+    }
+
+    private void setListener(ImageButton playPause) {
+        if (isPlaying) {
+            playPause.setOnClickListener(new PauseListener());
+            playPause.setImageResource(R.drawable.ic_pause_black_24dp);
+        } else {
+            playPause.setOnClickListener(new PlayListener());
+            playPause.setImageResource(R.drawable.ic_play_arrow_black_24dp);
+        }
     }
 }
