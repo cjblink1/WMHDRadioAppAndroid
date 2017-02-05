18e7c05eafee2432ea4cf4cccdc20500de032025
Connor Boyle <cjblink1@gmail.com>
2017-02-05 18:39:01 -0500

    Implemented initial week schedule view

---
+++ app/src/main/java/edu/rosehulman/boylecj/wmhdonlineradio/ScheduleComponents/DaySection.java
@@ -0,0 +1,76 @@
+package edu.rosehulman.boylecj.wmhdonlineradio.ScheduleComponents;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import edu.rosehulman.boylecj.wmhdonlineradio.R;
+import edu.rosehulman.boylecj.wmhdonlineradio.Show;
+import io.github.luizgrp.sectionedrecyclerviewadapter.StatelessSection;
+
+/**
+ * Created by Connor on 2/5/2017.
+ */
+
+public class DaySection extends StatelessSection {
+
+    private Show[] shows;
+    private String sectionName;
+
+
+    public DaySection(String sectionName, Show[] shows) {
+        super(R.layout.day_section_header, R.layout.day_section_item);
+        this.shows = shows;
+        this.sectionName = sectionName;
+    }
+
+    @Override
+    public int getContentItemsTotal() {
+        return this.shows.length;
+    }
+
+    @Override
+    public ItemViewHolder getItemViewHolder(View view) {
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindItemViewHolder(RecyclerView.ViewHolder holder, int position) {
+        ItemViewHolder itemHolder = (ItemViewHolder)holder;
+
+        itemHolder.mName.setText(shows[position].getName());
+    }
+
+    @Override
+    public RecyclerView.ViewHolder getHeaderViewHolder(View view) {
+        return new DaySection.HeaderViewHolder(view);
+    }
+
+    @Override
+    public void onBindHeaderViewHolder(RecyclerView.ViewHolder holder) {
+        HeaderViewHolder headerHolder = (HeaderViewHolder)holder;
+
+        headerHolder.mSectionName.setText(this.sectionName);
+    }
+
+    class ItemViewHolder extends RecyclerView.ViewHolder{
+
+        TextView mName;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            mName = (TextView)itemView.findViewById(R.id.section_item_name);
+        }
+    }
+
+    class HeaderViewHolder extends RecyclerView.ViewHolder{
+
+        TextView mSectionName;
+
+        public HeaderViewHolder(View itemView) {
+            super(itemView);
+            mSectionName = (TextView)itemView.findViewById(R.id.section_header_name);
+        }
+
+    }
+}
--- app/src/main/java/edu/rosehulman/boylecj/wmhdonlineradio/ScheduleComponents/WeekScheduleFragment.java
+++ app/src/main/java/edu/rosehulman/boylecj/wmhdonlineradio/ScheduleComponents/WeekScheduleFragment.java
@@ -3,6 +3,8 @@ package edu.rosehulman.boylecj.wmhdonlineradio.ScheduleComponents;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,12 +14,16 @@ import edu.rosehulman.boylecj.wmhdonlineradio.Constants;
 import edu.rosehulman.boylecj.wmhdonlineradio.GetWeekInfoTask;
 import edu.rosehulman.boylecj.wmhdonlineradio.R;
 import edu.rosehulman.boylecj.wmhdonlineradio.WeekInfoData;
+import io.github.luizgrp.sectionedrecyclerviewadapter.SectionedRecyclerViewAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WeekScheduleFragment extends Fragment implements GetWeekInfoTask.WeekInfoDisplayer {
 
+
+    private RecyclerView mRecyclerView;
+    private SectionedRecyclerViewAdapter mAdapter;
 
     public WeekScheduleFragment() {
         // Required empty public constructor
@@ -33,7 +39,8 @@ public class WeekScheduleFragment extends Fragment implements GetWeekInfoTask.We
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_week_schedule_component, container, false);
-
+        mRecyclerView = (RecyclerView)view.findViewById(R.id.week_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         return view;
 
     }
@@ -42,5 +49,22 @@ public class WeekScheduleFragment extends Fragment implements GetWeekInfoTask.We
     public void onWeekInfoLoaded(WeekInfoData wid) {
         Log.d(Constants.TAG, "WeekInfoData fetched");
         Log.d(Constants.TAG, "Tuesday shows: "+wid.getTuesday()[0].getName());
+        mAdapter = new SectionedRecyclerViewAdapter();
+
+        mAdapter.addSection(new DaySection("Monday", wid.getMonday()));
+        mAdapter.addSection(new DaySection("Tuesday", wid.getTuesday()));
+        mAdapter.addSection(new DaySection("Wednesday", wid.getWednesday()));
+        mAdapter.addSection(new DaySection("Thursday", wid.getThursday()));
+        mAdapter.addSection(new DaySection("Friday", wid.getFriday()));
+        mAdapter.addSection(new DaySection("Saturday", wid.getSaturday()));
+        mAdapter.addSection(new DaySection("Sunday", wid.getSunday()));
+        mAdapter.addSection(new DaySection("Next Monday", wid.getNextmonday()));
+        mAdapter.addSection(new DaySection("Next Tuesday", wid.getNexttuesday()));
+        mAdapter.addSection(new DaySection("Next Wednesday", wid.getNextwednesday()));
+        mAdapter.addSection(new DaySection("Next Thursday", wid.getNextthursday()));
+        mAdapter.addSection(new DaySection("Next Friday", wid.getNextfriday()));
+        mAdapter.addSection(new DaySection("Next Saturday", wid.getNextsaturday()));
+        mAdapter.addSection(new DaySection("Next Sunday", wid.getNextsunday()));
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
